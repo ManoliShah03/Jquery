200c1d345b26b69da1c73382caec5618d360f280
ManoliShah03 <manoli.shah@sigmainfo.net>
2023-03-21 10:17:40 +0530

    my second commit

--- AJAX/assignment2/edit.php
+++ AJAX/assignment2/edit.php
@@ -14,7 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             if ($updatedes != null && $updatepost != null && $updateuid != null) {
                 $query = "UPDATE Post SET userid='$updateuid' , Description='$updatedes', Title='$updatepost'  WHERE id='$id'";
                 if ($conn->query($query)) {
-                    $return_arr[] = array("message" => "user id,Post name and Description Updated");
+                    $return_arr[] = array("message" => "User Id, Post Name and Post Description Updated");
                     echo json_encode($return_arr);
                     exit;
                 }
@@ -28,14 +28,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             } else if ($updateuid != null) {
                 $query = "UPDATE Post SET userid='$updateuid' WHERE id='$id'";
                 if ($conn->query($query)) {
-                    $return_arr[] = array("message" => "user id Updated");
+                    $return_arr[] = array("message" => "User Id Updated");
                     echo json_encode($return_arr);
                     exit;
                 }
             } else if ($updatepost != null) {
                 $query = "UPDATE Post SET Title='$updatepost'  WHERE id='$id'";
                 if ($conn->query($query)) {
-                    $return_arr[] = array("message" => "Post name Updated");
+                    $return_arr[] = array("message" => "Post Name Updated");
                     echo json_encode($return_arr);
                     exit;
                 }
